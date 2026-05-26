35dab5e75790afcc66204d716b220edc4ee34835
Himanshi541 <your-email@example.com>
2026-05-26 12:06:28 +0530

    added show link and file to view detail passing id in path(route)

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,6 +1,16 @@
 class TodoListsController < ApplicationController
-
+  before_action :set_todo_list , only: [:show]
   def index
     @todo_lists = TodoList.all
   end
+
+  def show
+  end
+
+  private
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:id])
+  end
+
 end
